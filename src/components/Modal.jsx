0dd1f88985f716e91f27dcc32502b607d1778bfe
carrotpieOwO <xxhayoxx@naver.com>
2023-03-24 23:32:31 +0900

    모달 디자인 적용

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,38 +1,56 @@
 import styled from "styled-components"
-import { AnimatePresence, motion, useMotionValue, useScroll, useSpring, useTransform } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useModal } from "../contexts/ModalContexts";
 
-const Card = styled(motion.div)`
-    width: 80%;
-    border-radius: 20px;
-    height: 50vh;
-    background-image: url(${props => props.background});
-    background-size: cover;
-    overflow: hidden;
-    box-shadow: 0 5px 5px rgba(0,0,0,0.2);
-    text-align: center;
-    margin: 0 20px;
-    //transform: scale(${props => props.scale});
-    /* display: flex;
-    align-items: center;
-    justify-content: center; */
+const Circle = styled.div`
+    width: 15px;
+    height: 15px;
+    border-radius: 8px;
+    background-color: ${props => props.bgColor};
 `
-const OpenCard = styled(Card)`
-    /* width: 90vw;
-    height: 90vh; */
+const ModalContainer = styled(motion.div)`
     border-radius: 20px;
-    //top: 200vh;
-    background-color: #fff;
-    /* left: 5vw;
-    position: absolute; */
+    background-color: #202330;
     position: fixed;
     top: 10%;
-    left: 10%;
+    left: 8%;
     width: 80%;
     height: 80%;
     color: ${props => props.color};
     z-index: 9;
     overflow: auto;
+    padding: 0 40px;
+    color: #fff;
+    text-align: center;
+
+    img {
+        object-fit: fill;
+        max-width: 60%;
+        overflow: hidden;
+        border-radius: 10px;
+    }
+    p:last-child {
+        margin-bottom: 50px;
+    }
+`
+const ModalNav = styled.div`
+    display: flex;
+    position: fixed;
+    background-color: #202330;
+    width: 80%;
+    padding: 15px 0;
+    
+    div {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+    span {
+        position: absolute;
+        justify-content: center;
+        display: flex;
+        width: 100%;
+    }
 `
 const BackDrop = styled(motion.div)`
   position: fixed;
@@ -46,77 +64,58 @@ const CardWrap = styled.div`
     justify-content: center;
     align-items: center;
     gap: 20px;
-    flex-direction: ${props => props.flexColumn && 'column'};
+    margin: 70px 0 10px 0;
 `
-const Skill = styled(motion.span)`
-    gap: 20px;
-    margin-right: 50px;
-    margin-left: auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-`
-const Content = styled(Skill)`
-    align-items: center;
-    padding: 40px;
-`
+
 export default function Modal () {
     const { modal, setModal, content } = useModal();
-
     return (       
         <AnimatePresence>
             {
                 modal &&
                 <>
-                 <BackDrop
-                    onClick={() => setModal(false)}
-                    animate={{ opacity: 1 }}
-                    //exit={{ opacity: 0 }}
-                >
-                </BackDrop>
-                <OpenCard
-                initial={{opacity: 0, scale: 0}}
-                animate={{opacity: 1, scale: 1, transition: {duration: .3}}}
-                exit={{ opacity: 0, scale: 0, transition: {duration: .3}}}
-            >
-                { 
-                  content.skills.map((skill, i) => 
-                  <p key={`${skill}-${i}`}> 
-                      {skill}&nbsp; { i !== content.skills.length - 1 ? '|' : null }&nbsp;
-                  </p>
-                    )
-                }
-                {
-                    content.contents.map((obj, i) => 
-                    <div key={`obj${i}`}>
-                    <CardWrap flexColumn={obj.flexColumn}>
-                    {
-                        obj.images.map((item, i) => 
-                            <Card key={'image' + i} background={item.image} 
-                                size={item.size? item.size : [70, 60]}
-                            ></Card>
-                        )
-                    }
-                    </CardWrap>
-                    <Content>
-                    {
-                        obj.content.map((item, i) => 
-                            <p key={'content' + i}  >
-                                🐰 {item}
-                            </p>)
-                    }
-                    </Content>
-                    </div>
-                    )
-                }
-                
-                  
-            </OpenCard>
+                    <BackDrop
+                        onClick={() => setModal(false)}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                    >
+                    </BackDrop>
+                    <ModalContainer
+                        initial={{opacity: 0, scale: 0}}
+                        animate={{opacity: 1, scale: 1, transition: {duration: .3}}}
+                        exit={{ opacity: 0, scale: 0, transition: {duration: .3}}}
+                    >
+                        <ModalNav>
+                            <div>
+                                <Circle bgColor="#FF605C" />
+                                <Circle bgColor="#FFBD44" />
+                                <Circle bgColor="#00CA4E" />
+                            </div>
+                            <span>{content.projectNm}</span>
+                        </ModalNav>
+                        {
+                            content.contents.map((obj, i) => 
+                            <div key={`container-${i}`} >
+                                <CardWrap flexColumn={obj.flexColumn}>
+                                {
+                                    obj.images.map((item, i) => 
+                                        <img key={`image-${i}`} src={item.image} alt={item}/>
+                                    )
+                                }
+                                </CardWrap>
+                                {
+                                    obj.content.map((item, i) => 
+                                        <p key={`content-${i}`}>
+                                            - {item}
+                                        </p>
+                                    )
+                                }
+                            </div>
+                            )
+                        }                    
+                    </ModalContainer>
                 </>
-               
             }
-      
         </AnimatePresence>
-
     )
 }
