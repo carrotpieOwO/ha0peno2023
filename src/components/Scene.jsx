51b46284a9ddad435e1f99a26168c81ae17c21ec
carrotpieOwO <xxhayoxx@naver.com>
2023-03-26 23:11:43 +0900

    로딩화면 추가

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,13 +1,14 @@
-import { Cloud, OrbitControls, ScrollControls } from "@react-three/drei";
+import { Cloud, Html, OrbitControls, ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Character from "./Character";
+import Loading from "./Loading";
 import { Overlay } from "./Overlay";
 
 export default function Scene () {
     return (
-        <Canvas style={{height:'100vh',}} camera={{position: [0, 0.5, 1.5], rotate: [1, 0, 0]}} shadows >
-            <Suspense fallback={null}>
+        <Canvas style={{height:'100vh'}} camera={{fov:25,}} shadows >
+            <Suspense fallback={<Loading />}>
                 <OrbitControls enableZoom={false} />
                 <ambientLight intensity={.15} position={[0.07, 0.6, -1.5]} />
                 <pointLight intensity={2} position={[0, 0, -1000]} />
@@ -21,7 +22,7 @@ export default function Scene () {
                 <ScrollControls pages={4} damping={0.25}>
                     <Cloud speed={1} width={20} color='#FFAFBD' opacity={.5} />
                     <Overlay/>
-                    <Character receiveShadow castShadow position={[1.2, -1, 0.2]} rotation={[-0.5, -0.5, 0]} />
+                    <Character receiveShadow castShadow position={[1.2, -1, .4]} rotation={[0, -.2, 0]} />
                     <mesh
                         rotation={[-0.5 * Math.PI, 0, 0]}
                         position={[0, 0, 0]}
